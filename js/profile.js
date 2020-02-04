a2d59d82670a4287dbed8f27d2d673a933d855ad
Tom <tom.james15@hotmail.com>
2020-02-04 03:35:16 +0000

    media query for index

--- js/profile.js
+++ js/profile.js
@@ -38,19 +38,16 @@ var countDown = new Date($(".popup-bold-text").attr("data-time")).getTime();
 var countDownInterval = setInterval(function() {
     var now = new Date().getTime();
 
-  // Find the distance between now and the count down date
-  var distance = countDown - now;
+    var distance = countDown - now;
 
-  // Time calculations for days, hours, minutes and seconds
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  // Display the result in the element with id="demo"
-  $(".popup-bold-text").html(hours + "h "+ minutes + "m " + seconds + "s ");
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    $(".popup-bold-text").html(hours + "h "+ minutes + "m " + seconds + "s ");
 
-  // If the count down is finished, write some text
-  if (distance < 0) {
-    clearInterval(x);
-    $(".popup-bold-text").innerHTML = "EXPIRED";
-  }
+    // If the count down is finished, write some text
+    if (distance < 0) {
+        clearInterval(countDownInterval);
+        $(".popup-bold-text").html("EXPIRED");
+    }
 }, 1000);
