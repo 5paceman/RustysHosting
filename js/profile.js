d8e1ee1b8893c0be3ce143ed27ec8c6ee28528bd
Tom <tom.james15@hotmail.com>
2020-02-22 23:37:00 +0000

    countdown

--- js/profile.js
+++ js/profile.js
@@ -38,14 +38,18 @@ function ajaxForm(formEvent)
 }
 
 var countDown = new Date($(".popup-bold-text").attr("data-time")).getTime();
+console.log("Countdown: " + countDown);
 var countDownInterval = setInterval(function() {
     var now = new Date().getTime();
-
+    
     var distance = countDown - now;
 
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    console.log("H: " + hours);
+    console.log("M: " + minutes);
+    console.log("S: " + seconds);
     $(".popup-bold-text").html(hours + "h "+ minutes + "m " + seconds + "s ");
 
     // If the count down is finished, write some text
